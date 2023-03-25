a4008ee238879ddd20c957f7583f92cd22660fa7
yuichkun <yuichkun@gmail.com>
2023-03-26 03:19:27 +0900

    isolate TaskLineItem component

---
+++ src/TaskLineItem.tsx
@@ -0,0 +1,62 @@
+import { List, ActionPanel, Icon } from "@raycast/api";
+import { FC } from "react";
+import { HabiticaEditMenu } from "./actions/edit";
+import { determineColor } from "./date";
+import { nameToColor } from "./nameToColor";
+import { priorityToColor } from "./priorityToColor";
+import { HabiticaTask, Tag } from "./types";
+
+type Props = {
+  task: HabiticaTask;
+  refetchList: () => void;
+  allTags: Tag[];
+};
+
+export const TaskLineItem: FC<Props> = ({ task, refetchList, allTags }) => {
+  const tags = task.tags.map((tagId) => {
+    const found = allTags.find((tag) => tag.id === tagId);
+    if (!found) throw new Error(`${tagId} is not a valid tag id`);
+    return found;
+  });
+  return (
+    <List.Item
+      key={task.id}
+      title={task.text}
+      actions={
+        <ActionPanel title="Habitica">
+          <HabiticaEditMenu task={task} refetchList={refetchList} />
+        </ActionPanel>
+      }
+      accessories={[
+        ...tags.map((tag) => ({
+          tag: {
+            value: tag.name,
+            color: nameToColor(tag.name),
+          },
+        })),
+        {
+          date: task.date
+            ? {
+                color: priorityToColor(determineColor(task.date)),
+                value: new Date(task.date),
+              }
+            : null,
+        },
+      ]}
+      detail={
+        <List.Item.Detail
+          metadata={
+            <List.Item.Detail.Metadata>
+              <List.Item.Detail.Metadata.Label
+                title="Due Date"
+                text={task.date ?? "No Due Date"}
+                icon={Icon.Calendar}
+              />
+              <List.Item.Detail.Metadata.Separator />
+            </List.Item.Detail.Metadata>
+          }
+        />
+      }
+    />
+  );
+};
--- src/list-tasks.tsx
+++ src/list-tasks.tsx
@@ -1,13 +1,11 @@
-import { ActionPanel, Icon, List } from "@raycast/api";
+import { Icon, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
-import { FC, useState } from "react";
-import { HabiticaEditMenu } from "./actions/edit";
-import { determineColor, sortByDate } from "./date";
+import { useState } from "react";
+import { sortByDate } from "./date";
 import { getAllTags, retrieveTasks } from "./habitica";
 import { useSearch } from "./hooks/useSearch";
-import { nameToColor } from "./nameToColor";
-import { priorityToColor } from "./priorityToColor";
-import { HabiticaTask, Tag, HabiticaTaskTypes } from "./types";
+import { TaskLineItem } from "./TaskLineItem";
+import { HabiticaTaskTypes } from "./types";
 
 const Command = () => {
   const [taskType, setTaskType] = useState<HabiticaTaskTypes>("todo");
@@ -46,59 +44,6 @@ const Command = () => {
         <TaskLineItem key={task.id} task={task} refetchList={revalidate} allTags={allTags} />
       ))}
     </List>
-  );
-};
-
-const TaskLineItem: FC<{ task: HabiticaTask; refetchList: () => void; allTags: Tag[] }> = ({
-  task,
-  refetchList,
-  allTags,
-}) => {
-  const tags = task.tags.map((tagId) => {
-    const found = allTags.find((tag) => tag.id === tagId);
-    if (!found) throw new Error(`${tagId} is not a valid tag id`);
-    return found;
-  });
-  return (
-    <List.Item
-      key={task.id}
-      title={task.text}
-      actions={
-        <ActionPanel title="Habitica">
-          <HabiticaEditMenu task={task} refetchList={refetchList} />
-        </ActionPanel>
-      }
-      accessories={[
-        ...tags.map((tag) => ({
-          tag: {
-            value: tag.name,
-            color: nameToColor(tag.name),
-          },
-        })),
-        {
-          date: task.date
-            ? {
-                color: priorityToColor(determineColor(task.date)),
-                value: new Date(task.date),
-              }
-            : null,
-        },
-      ]}
-      detail={
-        <List.Item.Detail
-          metadata={
-            <List.Item.Detail.Metadata>
-              <List.Item.Detail.Metadata.Label
-                title="Due Date"
-                text={task.date ?? "No Due Date"}
-                icon={Icon.Calendar}
-              />
-              <List.Item.Detail.Metadata.Separator />
-            </List.Item.Detail.Metadata>
-          }
-        />
-      }
-    />
   );
 };
 
