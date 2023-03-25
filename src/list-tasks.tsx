b6318c3a21fb3bbd8289566579c1df2ff2a977f5
yuichkun <yuichkun@gmail.com>
2023-03-26 02:58:46 +0900

    add list dropdown

--- src/list-tasks.tsx
+++ src/list-tasks.tsx
@@ -1,15 +1,16 @@
 import { ActionPanel, Icon, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { HabiticaEditMenu } from "./actions/edit";
 import { determineColor, sortByDate } from "./date";
 import { getAllTags, retrieveTasks } from "./habitica";
 import { useSearch } from "./hooks/useSearch";
 import { nameToColor } from "./nameToColor";
 import { priorityToColor } from "./priorityToColor";
-import { HabiticaTask, Tag } from "./types";
+import { HabiticaTask, Tag, HabiticaTaskTypes } from "./types";
 
 const Command = () => {
+  const [taskType, setTaskType] = useState<HabiticaTaskTypes>("todo");
   const {
     isLoading: isAllTasksLoading,
     data: unfilteredTasks,
@@ -26,7 +27,21 @@ const Command = () => {
   if (allTags === undefined) return null;
 
   return (
-    <List isLoading={isAllTasksLoading || isAllTagLoading} onSearchTextChange={setSearchText}>
+    <List
+      isLoading={isAllTasksLoading || isAllTagLoading}
+      onSearchTextChange={setSearchText}
+      searchBarAccessory={
+        <List.Dropdown
+          tooltip="Set types of tasks to show"
+          onChange={(v: string) => setTaskType(v as HabiticaTaskTypes)}
+        >
+          <List.Dropdown.Section>
+            <List.Dropdown.Item title="To Do's" value="todo" icon={Icon.Pencil} />
+            <List.Dropdown.Item title="Dailies" value="daily" icon={Icon.Alarm} />
+          </List.Dropdown.Section>
+        </List.Dropdown>
+      }
+    >
       {filteredTasks.sort(sortByDate).map((task) => (
         <TaskLineItem key={task.id} task={task} refetchList={revalidate} allTags={allTags} />
       ))}
