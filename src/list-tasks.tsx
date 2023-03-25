d1abb501265fa01387afbdb2a9de260173d273a8
yuichkun <yuichkun@gmail.com>
2023-03-26 02:09:59 +0900

    enhance search by including tags

--- src/list-tasks.tsx
+++ src/list-tasks.tsx
@@ -1,7 +1,8 @@
 import { ActionPanel, Icon, List } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { HabiticaEditMenu } from "./actions/edit";
+import Fuse from "fuse.js";
 import { determineColor } from "./date";
 import { getAllTags, retrieveTasks } from "./habitica";
 import { nameToColor } from "./nameToColor";
@@ -11,7 +12,7 @@ import { HabiticaTask, Tag } from "./types";
 const Command = () => {
   const {
     isLoading: isAllTasksLoading,
-    data,
+    data: unfilteredTasks,
     revalidate,
   } = useCachedPromise(retrieveTasks, [], {
     initialData: [],
@@ -19,11 +20,42 @@ const Command = () => {
   const { isLoading: isAllTagLoading, data: allTags } = useCachedPromise(getAllTags, [], {
     keepPreviousData: true,
   });
+
+  const [searchText, setSearchText] = useState("");
+  const [filteredTasks, setFilteredTasks] = useState<HabiticaTask[]>(unfilteredTasks);
+
+  useEffect(() => {
+    if (searchText === "") {
+      setFilteredTasks(unfilteredTasks);
+      return;
+    }
+    const searchTargets = unfilteredTasks.map((task) => {
+      return {
+        ...task,
+        tags: task.tags.map((tagId) => {
+          const found = allTags?.find((tag) => tag.id === tagId);
+          if (!found) throw new Error(`${tagId} is not a valid tag`);
+          return found;
+        }),
+      };
+    });
+    const fuse = new Fuse(searchTargets, {
+      keys: ["text", "tags.name"],
+    });
+    const result = fuse.search(searchText);
+    setFilteredTasks(
+      result.map((r) => ({
+        ...r.item,
+        tags: r.item.tags.map((t) => t.id),
+      }))
+    );
+  }, [searchText]);
+
   if (allTags === undefined) return null;
 
   return (
-    <List isLoading={isAllTasksLoading || isAllTagLoading}>
-      {data.sort(sortByDate).map((task) => (
+    <List isLoading={isAllTasksLoading || isAllTagLoading} onSearchTextChange={setSearchText}>
+      {filteredTasks.sort(sortByDate).map((task) => (
         <TaskLineItem key={task.id} task={task} refetchList={revalidate} allTags={allTags} />
       ))}
     </List>
